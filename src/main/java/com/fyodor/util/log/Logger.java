199d415d375d5bf4dcbcf5eed5675eb202c0e540
fredisooon <fyodorstrujanov26@gmail.com>
2023-08-14 00:04:17 +0300

    refactor logging

--- src/main/java/com/fyodor/util/log/Logger.java
+++ src/main/java/com/fyodor/util/log/Logger.java
@@ -1,19 +1,37 @@
 package com.fyodor.util.log;
 
 public class Logger {
+    private static boolean loggingEnabled = true;
+
+    public static void setLoggingEnabled(boolean enabled) {
+        loggingEnabled = enabled;
+    }
+
     public static void logServerResponse(String message) {
-        System.out.println("[SERVER] " + message);
+        if (loggingEnabled) {
+            log("[SERVER]", message);
+        }
     }
 
     public static void logInfo(String message) {
-        System.out.println("[INFO] " + message);
+        if (loggingEnabled) {
+            log("[INFO]", message);
+        }
     }
 
     public static void logWarning(String message) {
-        System.err.println("[WARNING] " + message);
+        if (loggingEnabled) {
+            log("[WARNING]", message);
+        }
     }
 
     public static void logError(String message) {
-        System.err.println("[ERROR] " + message);
+        if (loggingEnabled) {
+            log("[ERROR]", message);
+        }
+    }
+
+    private static void log(String level, String message) {
+        System.out.println(level + " " + message);
     }
 }
